97ff26ac512cfdf47bfad8e8887c4f95065edf50
Nick Johnson <arachnid@notdot.net>
2019-05-30 17:53:52 +1200

    Comment out networks section

--- truffle.js
+++ truffle.js
@@ -13,11 +13,11 @@
  */
 
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
-    }
-  }
+  // networks: {
+  //   development: {
+  //     host: "127.0.0.1",
+  //     port: 8545,
+  //     network_id: "*"
+  //   }
+  // }
 };
